bd29278e575d1b75cea8e1f33dcb216f09b122fd
Johnp2185 <160353296+Johnp2185@users.noreply.github.com>
2024-02-20 01:27:02 -0500

    Add files via upload
    P1HW1 Python basic output with variables

---
+++ P1HW1_JohnPhillip.py
@@ -0,0 +1,23 @@
+# Phillip
+#20 Feb 24
+#P1HW1
+#Create a Python input and output with variables
+
+
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num) 
+
+print(user_num, 'squared is', user_num * user_num)
+
+print('And', user_num, 'cubed is', user_num * user_num * user_num, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))
+
+sum_num = user_num + user_num2
+
+product_num = user_num * user_num2 
+
+print(user_num, '+',user_num2, 'is', sum_num) 
+
+print(user_num, '*', user_num2, 'is', product_num)
