74d66fc1c27f838c5c759302b48172eacf2493a0
Johnp2185 <160353296+Johnp2185@users.noreply.github.com>
2024-02-20 00:29:08 -0500

    Update P1LAB1_JohnPhillip.py

--- P1LAB1_JohnPhillip.py
+++ P1LAB1_JohnPhillip.py
@@ -1,7 +1,7 @@
 #Phillip John
 #17 Feb 24
 #P1LAB1
-#Create a Python program using IDLE
+#Create a Python program using IDLE that takes a first name as the input, and outputs a welcome message to that name.
 
 first_name = input()
 print ('Hey', first_name)
